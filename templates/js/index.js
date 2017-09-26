cbba08139e7356df2717281ca6ebf32f0e6a2e40
Jason Josol <Jason Josol>
2017-09-26 15:52:02 +0800

    Changed file format versioning for interop

--- templates/js/index.js
+++ templates/js/index.js
@@ -1,10 +1,11 @@
 // References
 var express = require('express');
 var path = require('path');
+var package = require('../../../package.json');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-    res.sendFile('index.htm', { root: path.join(__dirname, '../views') });
+    res.render('index', { version: package.version.split('.').join('-') });
 });
 
 module.exports = router;
